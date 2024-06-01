7b36b54c9c74e976e61ecf7dc233e1cdfc811abf
ArturMatukiwa <artur.matukiwa@sptech.school>
2024-06-01 02:25:50 -0300

    Estilização da dashboard e uso de keyframes pelo site

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -1,10 +1,10 @@
 var rankingModel = require("../models/rankingModel");
 
 function ranquear(req, res) {
-    rankingModel.ranquear()
-    .then(function(resultado) {
+    rankingModel.ranquear().then(function(resultado) {
         console.log(`\nResultados encontrados: ${resultado.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultado)}`); // transforma JSON em String
+        console.log(`Resultados: ${JSON.stringify(resultado)}`); // transforma JSON em String
+
         res.status(200).json(resultado);
     }).catch(function(erro) {
         console.log(erro);
